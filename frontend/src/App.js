04d21d8c96f85b804aca0c5cef099c08db136e3c
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-06-19 10:21:31 +0200

    App : create header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Header from "./Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path={"/home"} component={Home} exact />
         <Route path={"*"} component={PageNotFound} />
