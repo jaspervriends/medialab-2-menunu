c70299ca1657926b9ee2b5ca6241b81fdf1516cf
Irene Prins <ireneprins11@hotmail.com>
2020-06-19 10:13:48 +0200

    add buttoncomponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import "./app.scss";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
