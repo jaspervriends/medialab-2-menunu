98291f0284087205b78d8d492389d6574532b45e
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-06-19 13:01:49 +0200

    App : created header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Header({onChange, opened}) {
+  return (
+    <div className={'header'}>
+      <div className={'header__button'} onClick={() => onChange(!opened)}>
+        <FontAwesomeIcon icon={["fas", "bars"]} />
+      </div>
+      <div className={'header__title'}>Menunu</div>
+    </div>
+  );
+}
