1f2fc219173ff541777b225d4407337aa87d2d34
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-06-23 13:40:53 +0200

    Menu : show data, edit

--- frontend/src/components/MenuCategory/MenuCategory.js
+++ frontend/src/components/MenuCategory/MenuCategory.js
@@ -2,27 +2,34 @@ import React, { useState } from "react";
 import "./menuCategory.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function MenuCategory() {
+function MenuCategory({edit = false, onCreate = () =>{}, menu}) {
   const [opened, setOpened] = useState(false);
+  const [createTitle, setCreateTitle] = useState("");
+  const [createPrice, setPriceTitle] = useState(0)
 
   return (
     <div className={"menu-category"}>
       <div className={"menu-category__header"} onClick={() => setOpened(!opened)}>
-        <h3 className={"menu-category__title"}>Restaurant titel</h3>
+      <h3 className={"menu-category__title"}>{menu.name}</h3>
         <div className={"menu-category__header-icon"}>
           <FontAwesomeIcon icon={["fas", "chevron-up"]} />
         </div>
       </div>
     
       {opened && (<div className={"menu-category-items"}>
+        {menu.items.map((item, key) => {
+          return <div key={key} className={"menu-category-item"}>
+            <span className={"menu-category-item__title"}>{item.name}</span>
+            <span className={"menu-category-item__prices"}>{item.price}</span>
+          </div>
+        })}
+        {edit && (
         <div className={"menu-category-item"}>
-            <span className={"menu-category-item__title"}>Kinderijsje</span>
-            <span className={"menu-category-item__prices"}>5 euro</span>
+          <input className={"menu-category-item__title"} value={createTitle} onChange={e => { setCreateTitle(e.target.value)}} />
+          <input type="number" className={"menu-category-item__prices"} value={createPrice} onChange={e => { setPriceTitle(e.target.value)}} />
+          <button onClick={() => {onCreate({name: createTitle, price: createPrice})}}><FontAwesomeIcon icon={["fas", "check"]} /></button>
         </div>
-        <div className={"menu-category-item"}>
-            <span className={"menu-category-item__title"}>Kinderijsje</span>
-            <span className={"menu-category-item__prices"}>5 euro</span>
-        </div>
+        )}
       </div>)}
 
     </div>
