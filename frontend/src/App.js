2a9ffb3eb9e7869afa5e5b2abf0a468ed6cf162d
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-06-23 10:22:52 +0200

    APP : add create page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav"
 import Restaurant from "./pages/Restaurant/Restaurant";
+import Create from "./pages/Create/Create";
 
 function App() {
   const [showNav, setShowNav] = useState(false);
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route path={"/home"} component={Home} exact />
         <Route path={"/restaurant"} component={Restaurant} />
+        <Route path={"/create"} component={Create} />
         <Route path={"*"} component={PageNotFound} />
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/pages/Create/Create.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./create.scss";
+import Tabmenu from "../../components/Tabmenu/Tabmenu";
+import MenuCategory from "../../components/MenuCategory/MenuCategory";
+
+export default function Restaurant() {
+  const [currentPage, setCurrentPage] = useState("menu");
+  const menuItems = [
+    {
+        title: "Info",
+        value: "info"
+    },
+    {
+        title: "Menu",
+        value: "menu"
+    },
+    {
+        title: "Beheer",
+        value: "beheer"
+    }
+];
+
+  return (
+    <div className={"container"}>
+      <div className={"restaurant"}>
+        <div className={"restaurant__header"}></div>
+        <Tabmenu onChange={(value) => setCurrentPage(value)} value={currentPage} menuItems={menuItems}/>
+      </div>
+
+      {currentPage === "info" && (
+          <div className={"restaurant__info"}>Het verhaal van Bram Ladage begon in 1967, met een druk bezochte patatkraam op de markt van Rotterdam.
+            Bram: ”Ik ben op de markt opgegroeid. Vier uur of half vijf opstaan en dan keihard aanpoten tussen de klapperende zeilen. Vanaf mijn elfde hielp ik mijn vader met de patat, vanaf mijn zestiende stond ik bij mijn moeder tussen de stoffen. Dat vond ik maar niks. Twee jaar later had ik mijn eigen patatkraam.
+            Inmiddels is Bram Ladage uitgegroeid tot een franchiseformule met 30+ filialen in Rotterdam en omstreken!
+          </div>
+      )}
+      {currentPage === "menu" && (
+          <div className={"restaurant__menu"}>
+            <MenuCategory />
+          </div>
+      )}
+      {currentPage === "beheer" && (
+          <div>beheer</div>
+      )}
+    </div>
+  );
+}
