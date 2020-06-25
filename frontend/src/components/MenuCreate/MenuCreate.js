b4cd2219a47769c821b69a7a8c8de30a467f2279
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-06-25 09:27:06 +0200

    MenuCreate : remove class

--- frontend/src/components/MenuCreate/MenuCreate.js
+++ frontend/src/components/MenuCreate/MenuCreate.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function MenuCreate({onChange}){
   const [createMenu, setCreateMenu] = useState('');
     return (
-      <div className={"restaurant__menu"}>
+      <div>
         <FontAwesomeIcon icon={["fas", "plus"]} role="button" onClick={() => onChange(createMenu)}/>
         <input className={"menu-category__create"} value={createMenu} onChange={e => {setCreateMenu(e.target.value)}} placeholder="Menu naam"/>
       </div>
